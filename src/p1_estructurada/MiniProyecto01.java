a1d67caa36064efc26ca64f76b790b70e3991394
zoe-alice <publicidad-aym@hotmail.com>
2025-04-16 10:10:05 -0500

    firt collaboration

---
+++ src/p1_estructurada/MiniProyecto01.java
@@ -0,0 +1,10 @@
+package p1_estructurada;
+
+public class MiniProyecto01 {
+public static void main(String[] args) {
+	String nombre="Julian Mati", tipoCuenta="debito";
+	double saldo=5500.20;
+	
+	System.out.println(saldo);
+}
+}
